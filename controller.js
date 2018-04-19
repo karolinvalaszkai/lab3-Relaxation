257998a1fec9b3ac6d0b40218d4ed1a385fe4c91
Karolin Valaszkai <karolinv@kth.se>
2018-04-19 12:07:21 +0200

    k about knapp funkar

--- controller.js
+++ controller.js
@@ -3,8 +3,8 @@ document.addEventListener('init', function(event) {
 
   if (page.id === 'pageMain') {
     page.querySelector('#push-button').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('about.html', {data: {title: 'about'}});
-    };
+      document.querySelector('#myNavigator').pushPage('about.html', {data: {title: 'About'}});
+    console.log("pushed button")};
   } else if (page.id === 'about') {
     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
   }
